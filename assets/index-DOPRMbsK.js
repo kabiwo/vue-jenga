6f4356a9c0647d1711bbae2d081efefaf4c67483
kabi <530853620@qq.com>
2025-09-05 20:19:24 +0800

    Updates

---
+++ assets/index-DOPRMbsK.js
@@ -0,0 +1,38 @@
+import{_ as t}from"./index.vue_vue_type_script_setup_true_lang-BPgZkEuw.js";import{d as n,i as a,e as o,o as m}from"./index-y3C4ZVii.js";const p=`# 组件介绍
+
+用于展示日期类表单组件，包含date与time，以及配置预处理的datetime、datetime-range、date-range
+
+# 使用方法
+
+在配置表单项时，以“time”作为type参数传入，则为时间选择，以“date”作为type参数传入，则为日期选择  
+
+另有预定义配置函数VjfcDateRange、VjfcDatetime、VjfcDatetimeRange分别提供日期范围、日期时间以及日期时间范围  
+
+参数类型如下：
+
+\`\`\`ts
+
+export type VjfTimeProps = VjFormItemBase & {
+  type: "time";
+  elTimeProps?: ElPropsType<typeof ElTimePicker>; // ElTime参数
+  elTimeEmit?: ElEventType<typeof ElTimePicker>;  // ElTime事件
+};
+
+export type VjfDateProps = VjFormItemBase & {
+  type: "date";
+  elDateProps?: ElPropsType<typeof ElDatePicker>; // ElDate参数
+  elDateEmit?: ElEventType<typeof ElDatePicker>;  // ElDate事件
+};
+
+const VjfcDateRange: (conf: Omit<VjfDateProps, "type">) => VjfDateProps
+
+const VjfcDatetime: (conf: Omit<VjfDateProps, "type">) => VjfDateProps
+
+const VjfcDatetimeRange: (conf: Omit<VjfDateProps, "type">) => VjfDateProps
+
+\`\`\`
+
+
+# 运行实例
+
+参看[VjForm实例1](/#/form/)中相关项`,l=n({__name:"index",setup(r){return(i,s)=>{const e=t;return m(),a(e,{mdStr:o(p)},null,8,["mdStr"])}}});export{l as default};
---
+++ assets/index-lktRsmwQ.js
@@ -0,0 +1,35 @@
+import{d as l,b as m,k as e,e as s,j as c,F as i,ae as u,o as _,a9 as d,cQ as f}from"./index-y3C4ZVii.js";import{_ as p}from"./index.vue_vue_type_script_setup_true_lang-BPgZkEuw.js";const g=`# 组件介绍
+
+VjConfirm是对ElMessageBox进行的二次封装，主要解决繁琐的配置以及promise处理，应对用户确认场景  
+
+# 使用方法
+
+直接传参使用，参看类型说明，返回值true表示点击确认，false取消
+
+# 类型说明
+
+\`\`\`ts
+const VjConfirm: (
+  title: string,  // 标题
+  message: string | VNode | (() => VNode),  // 内容，字符串/渲染函数/插槽
+  callback: () => Promise<boolean>, // 确认回调
+  options?: ElMessageBoxOptions,  // 原始组件选项暴露
+) => Promise<boolean>  // 返回true为确认，false为取消
+\`\`\`
+
+# 运行实例`,b=`\`\`\`html
+<el-button @click="confirm">操作确认</el-button>
+\`\`\`
+
+\`\`\`ts
+const confirm = async () => {
+  let res = await VjConfirm('操作确认', '123', () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(true);
+      }, 3000)
+    })
+  })
+  console.log(res);
+}
+\`\`\``,w=l({__name:"index",setup(k){const r=async()=>{let o=await f("操作确认","123",()=>new Promise(n=>{setTimeout(()=>{n(!0)},3e3)}));console.log(o)};return(o,n)=>{const t=p,a=u;return _(),m(i,null,[e(t,{mdStr:s(g)},null,8,["mdStr"]),e(a,{onClick:r},{default:c(()=>n[0]||(n[0]=[d("操作确认",-1)])),_:1,__:[0]}),e(t,{mdStr:s(b)},null,8,["mdStr"])],64)}}});export{w as default};
