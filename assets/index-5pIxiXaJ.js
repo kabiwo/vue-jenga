510fbd8a61bb62f3334bba5f0d0fceea2221d2f2
kabi <530853620@qq.com>
2025-09-05 16:18:50 +0800

    Updates

---
+++ assets/index-5pIxiXaJ.js
@@ -0,0 +1,92 @@
+import{G as r}from"./menu-ZH9nfHEQ.js";import{d as s,c as p,h as a,b as l,k as n,l as i,e as t,F as M,o as c,p as f}from"./index-CaRpFLgX.js";import"./el-popper-D_AwBTl1.js";import"./el-tooltip-l0sNRNKZ.js";import{_ as d}from"./index.vue_vue_type_script_setup_true_lang-BkQRyxLZ.js";import"./generator-Bb6p3Rz3-BTp7DydX.js";import"./string-CEni-xdW-dnqJ6hLG.js";const j=`# 组件介绍
+
+菜单组件VjMenu，是对ElMenu的二次封装，可以根据特定类型的对象数组渲染出菜单  
+
+提供类型VjMenuItem，VjMenuProps
+
+# 使用方法
+
+从vue-jenga包中引入VjMenu与所用类型，填写参数并在模板中使用
+
+\`\`\`html
+<VjMenu :menus :el-props="elProps"/>
+\`\`\`
+
+\`\`\`ts
+import { VjMenu, type VjMenuItem, type VjMenuProps } from 'vue-jenga/menu'
+\`\`\`
+
+# 外部交互
+
+组件通过defineExpose暴露内容与类型如下：
+\`\`\`ts
+const elMenuRef = ref<InstanceType<typeof ElMenu>>();
+
+defineExpose({
+  elMenuRef
+})
+\`\`\`
+
+# 类型说明
+
+\`\`\`ts
+import { type MenuProps, type MenuEmits, type MenuItemEmits } from 'element-plus'; 
+
+export type VjMenuItem = {  // 菜单配置项
+  name: string; // 菜单名
+  path: string; // 路由路径
+  icon?: string;  // 图标
+  group?: boolean;  // 分组情况
+  elItemProps?: Partial<MenuItemProps>; // ElMenuItem参数
+  elItemEmits?: Partial<MenuItemEmits>; // ElMenuItem事件
+  elSubProps?: Partial<SubMenuProps>; // ElSubMenu参数
+  children?: VjMenuItem[] | null;
+};
+
+export type VjMenuProps = { // 组件接收参数
+  elProps?: Partial<MenuProps>; // ElMenu参数
+  elEmits?: Partial<MenuEmits>; // ElMenu事件
+  menus: VjMenuItem[];  // 菜单配置项数组
+};
+\`\`\`
+
+# 运行实例`,P=`
+\`\`\`html
+<div class="vj-w-30%">
+  <VjMenu :menus :el-props="elProps"/>
+</div>
+\`\`\`
+
+\`\`\`ts
+import { computed, ref } from 'vue';
+import { VjMenu, type VjMenuItem, type VjMenuProps } from 'vue-jenga/menu'
+import { useRoute } from 'vue-router';
+
+const elProps = computed<VjMenuProps['elProps']>(() => ({
+  router: true,
+  defaultActive: useRoute()?.path
+}));
+
+const menus = ref<VjMenuItem[]>([
+  {
+    name: '概述',
+    path: '/',
+  }, 
+  {
+    name: 'Form',
+    path: '/form'
+  },
+  {
+    name: 'Menu',
+    path: '/menu'
+  },
+  {
+    name: 'Table',
+    path: '/table'
+  },
+  {
+    name: 'Utils',
+    path: '/utils'
+  }
+]);
+\`\`\``,V={class:"vj-w-30%"},R=s({__name:"index",setup(h){const o=p(()=>({router:!0,defaultActive:f()?.path})),u=a([{name:"概述",path:"/"},{name:"Form",path:"/form"},{name:"Menu",path:"/menu"},{name:"Table",path:"/table"},{name:"Utils",path:"/utils"}]);return(_,E)=>{const e=d,m=r;return c(),l(M,null,[n(e,{mdStr:t(j)},null,8,["mdStr"]),i("div",V,[n(m,{menus:u.value,"el-props":o.value},null,8,["menus","el-props"])]),n(e,{mdStr:t(P)},null,8,["mdStr"])],64)}}});export{R as default};
---
+++ assets/index-Dh6bUrQI.js
@@ -0,0 +1,50 @@
+import{_ as e}from"./index.vue_vue_type_script_setup_true_lang-BkQRyxLZ.js";import{d as t,i as o,e as s,o as r}from"./index-CaRpFLgX.js";const i=`# 组件介绍
+
+对axios进行了封装，提供配置管理与常用的Get/Post/Put/Del方法
+
+# 使用方法
+
+由于axios初始化不在响应式范围内，配置写在VjConf中调用不便，因此单独管理
+
+1. 在项目初始化时通过VjRequestInit方法传入配置
+
+2. 可通过VjRequestSetConf修改配置
+
+3. 调用Vj前缀的Get/Post/Put/Del方法
+
+# 类型说明
+
+\`\`\`ts
+export type VjRequestConf = { // 配置参数
+  // 请求拦截器
+  interceptorRequest: (conf: InternalAxiosRequestConfig) => InternalAxiosRequestConfig;
+  // 请求报错拦截器
+  interceptorRequestErr: (err: unknown) => unknown;
+  // 响应拦截器
+  interceptorResponse: (res: AxiosResponse) => AxiosResponse;
+  // 响应报错拦截器
+  interceptorResponseErr: (err: unknown) => unknown;
+  // 通用方法处理函数（按照各项目约定，实现自定义错误码处理等）
+  responseGlobalCheck: <T>(response: AxiosResponse<T, unknown>, options?: VjRequestOptions) => [true, T] | [false, unknown];
+}
+
+export type VjRequestOptions = AxiosRequestConfig & Record<string, unknown>;  // 请求参数
+
+const VjRequestSetConf: (conf: Partial<VjRequestConf>) => void  // 写入配置
+
+const VjRequestGetConf: () => Partial<VjRequestConf>   // 获取配置
+
+const VjRequestInit: (conf?: Partial<VjRequestConf> | undefined) => void  // 初始化拦截器，写入初始化配置
+
+// Get方法
+const VjGet: <T>(url: string, data?: unknown, options?: VjRequestOptions | undefined) => Promise<T>
+
+// Post方法
+const VjPost: <T>(url: string, data?: unknown, options?: VjRequestOptions | undefined) => Promise<T>
+
+// Put方法
+const VjPut: <T>(url: string, data?: unknown, options?: VjRequestOptions | undefined) => Promise<T>
+
+// Del方法
+const VjDel: <T>(url: string, data?: unknown, options?: VjRequestOptions | undefined) => Promise<T>
+\`\`\``,R=t({__name:"index",setup(u){return(p,a)=>{const n=e;return r(),o(n,{mdStr:s(i)},null,8,["mdStr"])}}});export{R as default};
