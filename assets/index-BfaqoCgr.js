05231677f7348c3279259a8ff725bb94872b8fa1
kabi <530853620@qq.com>
2025-09-18 19:41:23 +0800

    Updates

---
+++ assets/index-BfaqoCgr.js
@@ -0,0 +1,24 @@
+import{_ as o}from"./index.vue_vue_type_script_setup_true_lang-CvVgCYEb.js";import{d as t,i as e,e as r,o as s}from"./index-tVMZFrV7.js";const i=`# 组件介绍
+
+这里归纳一些对文件的操作，目前有：  
+
+VjDownFile： 下载文件
+
+VjBlobToUrl： Blob转url
+
+VjBlobToStr： Blob转文本
+
+# 使用方法
+
+直接调用
+
+# 类型说明
+
+\`\`\`ts
+const VjDownFile: (data: unknown, fileName: string, type?: string | undefined) => void
+
+const VjBlobToUrl: (data: unknown, type?: string | undefined) => string
+
+const VjBlobToStr: (blob: Blob, encode?: string | undefined) => Promise<string>
+\`\`\`
+`,_=t({__name:"index",setup(l){return(a,d)=>{const n=o;return s(),e(n,{mdStr:r(i)},null,8,["mdStr"])}}});export{_ as default};
